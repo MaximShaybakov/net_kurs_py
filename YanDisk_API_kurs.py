2a72e1dc54631ed465d3d0307de6d3b840c7967f
Maxim Shaybakov <momon_boton@mail.ru>
2022-04-11 02:14:18 +0500

    Last commit

---
+++ YanDisk_API_kurs.py
@@ -0,0 +1,83 @@
+import os
+import requests
+from pprint import pprint
+import time
+from tqdm import tqdm
+
+
+def run():
+    for i in tqdm('__main__', desc='Sending data to Yandex disk'):
+        time.sleep(2)
+    ProfileYD = WriteYaDisk()
+    ProfileYD.upload_file_to_disk()
+
+
+class WriteYaDisk:
+
+    def __init__(self): # инициализация класса для работы с api yandex disk
+        self.token = self.get_token()
+        self.url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+
+    def get_token(self): # чтение токена для api yandex disk
+        with open('tokens.txt') as file:  # укажите путь к файлу с вашим токеном к API VK
+            token_ya = file.readlines()[1].strip()
+        return token_ya
+
+
+    def get_resp(self): # получение json всех данных на диске (печать на экран не обязательна)
+        try:
+            resp = requests.get(self.url, headers=self.headers, timeout=5)
+            return resp.json()
+        except KeyError:
+            print('>>>>> [!] Invalid token Yandex. <<<<<')
+            exit()
+
+
+    def get_upload_link(self):
+        '''
+        Получение пути на яндекс.диск для загрузки Вашего файла.
+        '''
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/"
+        headers = self.headers
+        link_json_list = []
+        with open('photo/photo_info.json') as file:
+            info = file.read()
+        num_info = 0
+        for name in self.write_photo():
+            params = {"path": f'disk:/{name.strip()}', "overwrite": "true"}
+            response = requests.get(upload_url + 'upload', headers=headers, params=params, timeout=10)
+            link_json_list.append(response.json())
+            num_info += 1
+        return link_json_list
+
+
+    def write_photo(self):
+        with open('photo/all_names.txt', 'r', encoding='utf-8') as file:
+            ls_name_photo = file.readlines()
+        return ls_name_photo
+
+
+    def upload_file_to_disk(self):
+        '''
+        Загрузка файла на яндекс диск.
+        '''
+        href = self.get_upload_link()
+        headers = self.headers
+        print('Copying files. Wait please...')
+        val_photo = 0
+        for name in self.write_photo():
+            response = requests.put(href[val_photo].get("href", ""), data=open(f'photo/{name.strip()}', 'rb'))
+            val_photo += 1
+        if response.status_code == 201:
+            print('Success!')
+        else:
+            print(f'Error {response.status_code}.')
+
+
+if __name__ == '__main__':
+    run()
---
+++ main.py
@@ -0,0 +1,104 @@
+import json
+import requests
+from pprint import pprint
+import time
+from tqdm import tqdm
+from YanDisk_API_kurs import run
+
+
+def show_bar(): # функция запускает всю логику программы
+    Profile_1 = APIVk()
+    Profile_1.download_photo()
+    for i in tqdm('__main__', desc='Getting data from VK API'):
+        time.sleep(0.8)
+
+
+class APIVk: # инициализация класса
+    def __init__(self):
+        self.token = self.get_token()
+        self.ID = int(input('Enter target ID: '))
+
+
+    def get_token(self): # чтение токена из фа706йла
+        with open('tokens.txt', 'rb') as file:  # укажите путь к файлу с вашим токеном к API VK
+            token_vk = file.readlines()[0].strip()
+        return token_vk
+
+
+    def get_headers(self, ID=552934290): # параметры запроса к api vk метода photos.get для ID 552934290
+        return {
+            'access_token': self.token, #токен обновлять ежедневно с разрешением на фото
+            'owner_id': f'{self.ID}',
+            'album_id': 'profile',
+            'rev': '0',
+            'extended': '1',
+            'photo_size': '1',
+            'v': '5.81'
+        }
+
+
+    def req_vk(self): #получение json файла фотографий профиля с API VK
+        url = f'https://api.vk.com/method/photos.get'
+        params = self.get_headers()
+        response = requests.get(url, params=params, timeout=5)
+        return response.json()
+
+
+    def url_photo_max_size(self): # список url фото максимального размера
+        ls_photo_max_size = []
+        try:
+            for url_photo in self.req_vk()['response']['items']:
+                ls_photo_max_size.append(url_photo['sizes'][-1]['url'])
+            return ls_photo_max_size
+        except KeyError:
+            print('>>>>> [!] Invalid token VK <<<<<')
+            exit()
+
+
+    def get_info_json(self): # создание файла json с данными о фото
+        ls_json_info = []
+        ls_name = []
+        for val in self.req_vk()['response']['items']:
+            if val['likes']['count'] not in ls_name:
+                result = {
+                    'name': f"{val['likes']['count']}",
+                    'size': f"{val['sizes'][-1]['type']}",
+                    'url': f"{val['sizes'][-1]['url']}"
+                }
+            else:
+                result = {
+                    'name': f"{val['date']}",
+                    'size': f"{val['sizes'][-1]['type']}",
+                    'url': f"{val['sizes'][-1]['url']}"
+                }
+            ls_name.append(val['likes']['count'])
+            ls_json_info.append(result)
+        with open('photo/photo_info.json', 'w', encoding='utf-8') as file: # запись данных в файл
+            json.dump(ls_json_info, file, ensure_ascii=False, indent=4)
+        return ls_json_info
+
+
+    def ls_name(self): # создание списка с именами файлов и их запись в файл
+        ls_name_photo = []
+        with open('photo/all_names.txt', 'w') as file:
+            for name_photo in self.get_info_json():
+                ls_name_photo.append(name_photo['name'])
+                file.write(name_photo['name'] + '.jpeg\n')
+        with open('photo/all_names.txt', 'a') as f:
+            f.write('photo_info.json')
+        return ls_name_photo
+
+
+    def download_photo(self): #загрузка фото с сервера на локальный диск в папку
+        counter = 0
+        for photo in self.url_photo_max_size():
+            img = requests.get(photo)
+            with open(f"photo/{self.ls_name()[counter]}.jpeg", "wb") as file:
+                file.write(img.content)
+                counter += 1
+        return
+
+
+if __name__ == '__main__':
+    show_bar()
+    run()
